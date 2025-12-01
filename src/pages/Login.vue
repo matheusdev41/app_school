fdee5415f6ed6242c75aa59da2c121055d280445
Matheus Pessoa <matheus.dev@gmail.com>
2025-12-01 14:28:43 -0300

    first commit

---
+++ src/pages/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="login">
+        <h1>Login Aluno</h1>
+        <form @submit.prevent="fazerLogin">
+            <input type="text" v-model="usuario" placeholder="Usuário" required>
+            <input type="password" v-model="senha" placeholder="Senha" required>
+            
+        </form>
+    </div>
+</template>
+
+<script setup> 
+    function fazerLogin(){
+
+    }
+</script>
