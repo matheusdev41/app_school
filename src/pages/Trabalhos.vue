bed3476c15def741589741d417a1b7cf78fc2ac0
Matheus Pessoa <matheus41.dev@gmail.com>
2025-12-15 08:23:13 -0300

    page trabalhos

--- src/pages/Trabalhos.vue
+++ src/pages/Trabalhos.vue
@@ -1,8 +1,16 @@
 <template>
     <div class="trabalhos-container">
         <h2 class="text-5 font-weight-bold">📘 Trabalhos</h2>
-        <v-container class="py-8">
 
+        <v-container class="py-8">
+            <!-- BOTÃO NOVO TRABALHO -->
+             <v-btn
+             color="primary"
+             class="mt-4"
+             pretend-icon="mdi-plus"
+             >
+                Novo Trabalho
+            </v-btn>
         </v-container>
     </div>
 </template>
